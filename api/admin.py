664c2814a1999c23da2c6249f4e12f154adce054
demaxl <aweabdullahi@gmail.com>
2023-10-28 19:15:29 +0100

    Added profile create signals and string representation of models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article, Comment, Reply, Profile
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Reply)
+admin.site.register(Profile)
--- api/models.py
+++ api/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.validators import MinValueValidator
 
+DEFAULT_IMG_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", primary_key=True)
-    date_joined = models.DateField(auto_now_add=True)
-    bio = models.TextField(max_length=500)
-    image = models.URLField()
+    bio = models.TextField(max_length=500, blank=True)
+    image = models.URLField(default=DEFAULT_IMG_URL)
+
+    def __str__(self) -> str:
+        return f"{self.user}'s profile"
 
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
@@ -15,6 +19,9 @@ class Article(models.Model):
     time_posted = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="liked_articles")
 
+    def __str__(self):
+        return f"{self.title}"
+
 class BaseComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(max_length=300)
@@ -22,6 +29,9 @@ class BaseComment(models.Model):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return f"{self.user}: {self.body[:20]}..."
 
 class Comment(BaseComment):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
