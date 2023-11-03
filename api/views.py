d06943ee1c263688bc455ec5ae261862c86c90f6
demaxl <aweabdullahi@gmail.com>
2023-11-03 20:14:33 +0100

    Create base comment viewset

--- api/views.py
+++ api/views.py
@@ -60,34 +60,53 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
         msg = article.like(request.user)
         return Response({"success":True, "Message":msg})
-     
 
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
+
+class BaseCommentViewSet(viewsets.ModelViewSet) :
     permission_classes = [IsCommenterOrReadOnly]
-   
+
     def dispatch(self, request, *args, **kwargs):
         article_id = kwargs.get('article_pk')
+        comment_id = kwargs.get("comment_pk")
 
         if article_id and not article_id.isdigit():
             kwargs.update({"article_pk":-1})
 
-        return super().dispatch(request, *args, **kwargs)
+        if comment_id and not comment_id.isdigit:
+            kwargs.update({"comment_pk":-1})
 
-    def get_queryset(self):
-        article = get_object_or_404(Article, pk=self.kwargs['article_pk'])
-        queryset = Comment.objects.filter(article=article)
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_queryset(self, parent, model, pk):
+        parent_model = get_object_or_404(parent, pk=pk)
+        queryset = model.objects.filter(**{parent.__name__.lower():parent_model})
         return queryset
     
-    def perform_create(self, serializer):
-        article = get_object_or_404(Article, pk=self.kwargs['article_pk'])
-        serializer.save(user=self.request.user, article=article)
+    def perform_create(self, serializer, parent, pk):
+        parent_model = get_object_or_404(parent, pk=pk)
+        serializer.save(user=self.request.user, **{parent.__name__.lower():parent_model})
 
+    
+
+class CommentViewSet(BaseCommentViewSet):
+    serializer_class = CommentSerializer
+   
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(
+            Article, Comment, self.kwargs['article_pk']
+            )
+    
+    def perform_create(self, serializer, *args, **kwargs):
+        return super().perform_create(
+            serializer, Article, self.kwargs['article_pk']
+        )
+    
     @action(detail=True, methods=["POST"], permission_classes=[IsAuthenticated])
     def like(self, request, *args, **kwargs):
-        comment = self.get_object()
+        instance = self.get_object()
 
-        msg = comment.like(request.user)
+        msg = instance.like(request.user)
         return Response({"success":True, "Message":msg})
 
     
